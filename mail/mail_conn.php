7f3c7002d256f63cbb69c5b7fb944bf1f4c34c94
stofio <54468433+stofio@users.noreply.github.com>
2022-03-03 23:02:36 +0100

    smtp setup, working

--- mail/mail_conn.php
+++ mail/mail_conn.php
@@ -11,15 +11,32 @@ $mail->CharSet = 'UTF-8';
 $mail->IsSMTP();
 $mail->Mailer = "smtp";
 
-$mail->SMTPDebug  = 1;  
+$mail->SMTPDebug  = 2;  
 $mail->SMTPAuth   = TRUE;
-$mail->SMTPSecure = "tls";
-$mail->Port       = 587;
+$mail->SMTPSecure = "ssl";
+$mail->Port       = 465;
 $mail->Host       = "smtp.gmail.com";
 $mail->Username   = "dev.test.dejan@gmail.com";
-$mail->Password   = "Beeg.com995a**";
+$mail->Password   = "qmcbrnbedwifplya";
 
-$from = "dev.test.dejan@gmail.com";
+
+$mail->IsHTML(true);
+$mail->AddAddress("stofio@live.com", "recipient-name");
+$mail->SetFrom("dev.test.dejan@gmail.com", "from-name");
+$mail->AddReplyTo("sghefio@live.com", "reply-to-name");
+$mail->AddCC("sghefio@live.com", "cc-recipient-name");
+$mail->Subject = "Test is Test Email sent via Gmail SMTP Server using PHP Mailer";
+$content = "<b>This is a Test Email sent via Gmail SMTP Server using PHP mailer class.</b>";
+
+$mail->MsgHTML($content); 
+if(!$mail->Send()) {
+  echo "Error while sending Email.";
+  var_dump($mail);
+} else {
+  echo "Email sent successfully";
+}
+
+
 
 
 ?>
